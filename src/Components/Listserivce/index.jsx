b2bc5676c9ae56668543d88667f4c4708b1f7b2e
your name <duythai147258@gmail.com>
2022-02-14 15:28:13 +0700

    Reactjs

--- src/Components/Listserivce/index.jsx
+++ src/Components/Listserivce/index.jsx
@@ -50,10 +50,84 @@ function Listserivce(){
                                     <input type="text" value={codeService} onChange={ e=> setCodeService(e.target.value)}  />
                                 </div>
                             </div>
+                            
+                            <div className={classes.serviceCode}>
+                                <div className={classes.serviceCodetitle}>
+                                    Tên dịch vụ: <span>*</span>
+                                </div>
+                                <div className={classes.serviceCodeinput}>
+                                    <input type="text" value={codeService} onChange={ e=> setNameService(e.target.value)}  />
+                                </div>
+                            </div>       
                         </div>
+                        <div className={classes.serviceCode}>
+                                <div className={classes.serviceCodetitle}>
+                                    Mô tả: <span>*</span>
+                                </div>
+                                <div className={classes.serviceCodeinput}>
+                                    <textarea rows={4} cols={50} placeholder="Mô tả dịch vụ" value={desService} onChange={ e=> setDesService(e.target.value)}></textarea>
+                                </div>
+                        </div>
+                    </div>
+
+                    <div className={classes.contaniner__rules}>
+                        <div className={classes.contaniner__rules__title}>
+                                Quy tắc cấp số
+                        </div>
+                        <div className={classes.contaniner__checkbox}>
+                            <div className={classes.contaniner__checkbox__items}>
+                                <div className={classes.contaniner__checkbox__item}>
+                                    <input type="checkbox"/> 
+                                    <span>Tăng tự động từ:</span>
+                                </div> 
+                                <div className={classes.contaniner__checkbox__item}>
+                                    <input type="checkbox"/> 
+                                    <span>Prefix:</span>
+                                </div>   
+                                <div className={classes.contaniner__checkbox__item}>
+                                    <input type="checkbox"/> 
+                                    <span>Surfix:</span>
+                                </div>   
+                                <div className={classes.contaniner__checkbox__item}>
+                                    <input type="checkbox"/> 
+                                    <span>Reset mỗi ngày</span>
+                                </div>   
+                            </div>
+                                <div className={classes.contaniner__input__items}>
+                                <div className={classes.contaniner__input__item}>
+                                    <input type="text" value='0001'  onChange={ e=>'value'}/>
+                                    <span>đến</span>
+                                    <input type="text" value='9999' onChange={ e=>'value'}/>
+                                </div>
+                                <div className={classes.contaniner__input__item}>
+                                    <input type="text" value='0001'  onChange={ e=>'value'}/>                               
+                                </div>
+                                
+                                <div className={classes.contaniner__input__item}>
+                                    <input type="text" value='0001'  onChange={ e=>'value'}/>                               
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className={classes.contaniner__more}>
+                        <div className={classes.contaniner__more__title}>
+                            <p className={classes.star}>*</p> <span className={classes.contaniner__more__require}>Là trường thông tin bắt buộc</span> 
+                        </div>
+                    </div>
+
+                </div>
+                <div className={classes.contaniner__button}>
+                    <div className={classes.contaniner__cancel}>
+                    <Link to="/service"><button>Hủy bỏ</button></Link>
+                    </div>
+                    <div className={classes.contaniner__addservice}>
+                        <button>Thêm dịch vụ</button>
                     </div>
                 </div>
             </div>
+                
         </div>
     )
 }
